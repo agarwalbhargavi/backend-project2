0b17f060b6eba97694efe2a0ea871842ee4e722f
Bhargavi Agarwal <agarwalbhargavi07@gmail.com>
2026-03-13 21:20:44 +0530

    testing

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const connectToDB= require("./src/config/database")
 connectToDB()
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port 3000.")
 })

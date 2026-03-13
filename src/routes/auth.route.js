b085e843d61d255d820151ae89d19fbcfa47da52
Bhargavi Agarwal <agarwalbhargavi07@gmail.com>
2026-03-13 18:52:01 +0530

    my third commit

---
+++ src/routes/auth.route.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const authController = require("../controllers/auth.controller")
+
+
+const authRouter = express.Router()
+
+
+authRouter.post("/register",authController.registerUserController)
+
+
+authRouter.post("/login",authController.loginUserController)
+
+
+authRouter.get("/logout",authController.logoutUserController)
+ 
+module.exports = authRouter
